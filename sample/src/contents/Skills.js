45dff940d1198b9d5ce2eeafb34cbb91ab2a3472
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-05-05 08:32:53 -0500

    function embedding

--- sample/src/contents/Skills.js
+++ sample/src/contents/Skills.js
@@ -9,14 +9,11 @@ class Skills extends Component {
         };
     }
 
-    getVPageData() {
-        return "Skills";
-    }
-
     render() {
         return (
             <div className="condiv skills">
             <h1 className="subtopic">My Skills</h1>
+            { function getVPageData() { return "Skills"; }}
             <ul>
             {this.state.myskills.map((value)=>{
                 return <li>{value}</li>
